58558da82d36890d1f52678e66880b726f26f882
Gary Huong <garyhuong@gmail.com>
2021-03-05 16:31:08 -0500

    Implement Exercise 3.15: phonebook database step3

--- index.js
+++ index.js
@@ -48,15 +48,16 @@ app.get("/api/persons", (request, response) => {
 });
 
 // get person
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 // get info
@@ -93,11 +94,36 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+// delete person
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
+
+// unknown endpoint middleware
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({
+    error: "Unknown endpoint",
+  });
+};
+
+app.use(unknownEndpoint);
+
+// error handler
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformatted id " });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
