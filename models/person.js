fbb6bcd40f9ebde6d36d7cef66520af42802f5ed
Gary Huong <garyhuong@gmail.com>
2021-03-05 17:46:11 -0500

    Enforce name minlength 3, and number minlength 8

--- models/person.js
+++ models/person.js
@@ -22,12 +22,14 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    minlength: 3,
   },
   number: {
     type: String,
     required: true,
-  }
+    minlength: 8,
+  },
 });
 personSchema.plugin(uniqueValidator);
 
